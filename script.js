774b512c2391b1f8661bea43dfd565c4b184250b
gitdishant <92519667+gitdishant@users.noreply.github.com>
2025-07-20 23:19:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
+  event.preventDefault(); // Prevent form from submitting
+  
+  // Get form elements
+  const form = document.getElementById('form1');
+  const firstName = form.elements['fname'].value;
+  const lastName = form.elements['lname'].value;
 
+  // Show the full name
+  alert(firstName + " " + lastName);
 }
+
+
+
